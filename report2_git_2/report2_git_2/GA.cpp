03cf358f623cc58c35c3492da1e95f980fe9bd63
Akira Kumashiro <34697080+akira-kumashiro@users.noreply.github.com>
2018-05-15 16:37:13 +0900

    提出版

--- report2_git_2/report2_git_2/GA.cpp
+++ report2_git_2/report2_git_2/GA.cpp
@@ -6,14 +6,12 @@ GA::GA(int _max_genom_list, int _var_num, std::vector<double> _varMax, std::vect
 	eliteData(_var_num)
 {
 	//もらった変数をクラス内変数に格納
-	max_genom_list = _max_genom_list;
-	var_num = _var_num;
 	varMax = _varMax;
 	varMin = _varMin;
 
-	for (int i = 0; i < max_genom_list; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
-		for (int j = 0; j < var_num; j++)
+		for (int j = 0; j < data[i].x.size(); j++)
 		{
 			data[i].x[j] = random(varMin[j], varMax[j]);//遺伝子の初期設定
 		}
@@ -30,16 +28,9 @@ bool GA::selection()
 	bool ret = isChanged;//最も評価の良い個体の変化の監視(デバッグ用)
 	isChanged = false;
 
-	/*for (int i = 0; i < max_genom_list; i++)//ルーレット選択用に評価関数の合計と一番評価の良い番号を取得
-	{
-		if (data[i].result > data[max_num].result)
-			max_num = i;
-	}
-
-	eliteData = data[max_num];*/
 	eliteData = searchRank(0);//最も評価の良い個体を保持
 	prev_data = data;
-	for (int i = 0; i < max_genom_list; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
 		double selector = random(0.0, 1.0);//乱数を生成
 		double needle = 0;//ルーレットの針を生成
@@ -49,7 +40,7 @@ bool GA::selection()
 			needle += (prev_data[j].result / resultSumValue);//ルーレットの針を乱数の値まで進める
 			if (needle > selector)
 				break;
-			if (j == (max_genom_list - 1))
+			if (j == (data.size() - 1))
 				break;
 		}
 		data[i] = prev_data[j];
@@ -57,13 +48,13 @@ bool GA::selection()
 	return ret;
 }
 
-bool GA::blxAlphaCrossover()
+void GA::blxAlphaCrossover()
 {
 	prev_data = data;
 
-	for (int i = 0; i < max_genom_list; i += 2)//2個ずつ交叉
+	for (int i = 0; i < data.size(); i += 2)//2個ずつ交叉
 	{
-		for (int j = 0; j < var_num; j++)
+		for (int j = 0; j < data[i].x.size(); j++)
 		{
 			double ave = (data[i].x[j] + data[i + 1].x[j]) / 2;
 			double length = std::abs((data[i].x[j] - data[i + 1].x[j]));
@@ -72,45 +63,41 @@ bool GA::blxAlphaCrossover()
 			data[i + 1].x[j] = random(ave - length * (1 + alpha * 2) / 2, ave + length * (1 + alpha * 2) / 2);
 		}
 	}
-	return true;
 }
 
-bool GA::mutation()
+void GA::mutation()
 {
-	for (int i = 0; i < max_genom_list; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
 		if (random(0.0, 1.0) <= individualMutationRate)//個体突然変異率の計算
 		{
-			int pos = random(0, var_num - 1);
+#ifdef __ENABLE_SINGLE_POINT_MUTATION__
+			int pos = random(0, (int)data[i].x.size() - 1);
 			data[i].x[pos] += random(varMin[pos] * random(0, genomMutationRate), varMax[pos] * random(0, genomMutationRate));
-
-			//			for (int j = 0; j < var_num; j++)
-			//			{
-			//				data[i].x[j] = random(varMin[j], varMax[j]);
-			//			}
+#else
+			for (int j = 0; j < data[i].x.size(); j++)
+			{
+				if (random(0.0, 1.0) <= genomMutationRate)
+					data[i].x[j] = random(varMin[j], varMax[j]);
+			}
+#endif
 		}
 	}
-	return true;
 }
 
-bool GA::calc(bool enableDisplay, bool enableOneLine)
+void GA::calc(bool enableDisplay, bool enableOneLine)
 {
+	int minNum = 0, maxNum = 0;
 	calcResult();
-	for (int i = 0; i < max_genom_list; i++)//評価関数が最小の奴と最大のやつを検索
+	for (int i = 0; i < data.size(); i++)//評価関数が最小の奴と最大のやつを検索
 	{
 		if (data[i].result < data[minNum].result)
-		{
 			minNum = i;
-		}
 		if (data[i].result > data[maxNum].result)
-		{
 			maxNum = i;
-		}
 	}
-	if (data[maxNum].functionValue - eliteData.functionValue > 0.0001)
-	{
+	if (data[maxNum].functionValue - eliteData.functionValue > 0.00001)
 		isChanged = true;
-	}
 	//評価関数が最もいいやつを保存
 	data[minNum] = eliteData;
 
@@ -118,39 +105,34 @@ bool GA::calc(bool enableDisplay, bool enableOneLine)
 
 	if (enableDisplay)
 		displayValues(enableOneLine);
-
-	return true;
 }
 
-bool GA::calcResult(bool enableSort)
+void GA::calcResult(bool enableSort)
 {
 	int maxNum = 0;
 
-	for (int i = 0; i < max_genom_list; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
 		data[i].functionValue = std::sin(data[i].x[0] + data[i].x[1]) + std::pow((data[i].x[0] - data[i].x[1]), 2.0) - 1.5*data[i].x[0] + 2.5*data[i].x[1] + 1;//与えられた関数
 
-		 //min:(-0.54719,-1.54719)=-1.9133//min:(-0.54719,-1.54719)=-1.9133
 		if (data[maxNum].functionValue < data[i].functionValue)//座標の中で最も関数が大きいやつを検索
-		{
 			maxNum = i;
-		}
 	}
 	double seg = data[maxNum].functionValue;//評価関数の切片を与えられた関数が最も大きいやつにセット
-	double seg2 = searchRank(var_num - 2).functionValue - seg;
+	double seg2 = searchRank(data[0].x.size() - 2).functionValue - seg;
 	resultSumValue = 0;
-	double coefficient = 0.001 / var_num;//評価関数用の定数
+	double coefficient = 0.001 / data[0].x.size();//評価関数用の定数
 
-	for (int i = 0; i < max_genom_list; i++)
+	for (int i = 0; i < data.size(); i++)
 	{
 		bool flag = true;
-		
-		for (int j = 0; j < var_num; j++)
+
+		for (int j = 0; j < data[i].x.size(); j++)
 		{
 			if (data[i].x[j] > varMax[j] || data[i].x[j] < varMin[j])//座標が場外にいるやつの処理
 				flag = false;
 		}
-		data[i].result = std::pow((data[i].functionValue - seg)/seg2/coefficient, 2.0);//与えられた関数の値から切片で設定した値を引いて2乗する→与えられた関数の値が小さいやつが強くなる
+		data[i].result = (data[i].functionValue - seg) / seg2 / coefficient;//与えられた関数の値から切片で設定した値を引いて2乗する→与えられた関数の値が小さいやつが強くなる
 		//data[i].result = std::abs(data[i].functionValue - seg);
 
 		if (!flag)//場外に出たやつの処理
@@ -159,8 +141,6 @@ bool GA::calcResult(bool enableSort)
 	}
 	if (enableSort)
 		std::sort(data.begin(), data.end(), [](const Data& x, const Data& y) { return x.functionValue > y.functionValue; });
-
-	return true;
 }
 
 int GA::random(int min, int max)
@@ -172,16 +152,8 @@ int GA::random(int min, int max)
 	return distribution(engine);
 }
 
-double GA::random(int min, double max)
-{
-	return random((double)min, max);
-}
-
-double GA::random(double min, int max)
-{
-	return random(min, (double)max);
-}
-
+double GA::random(int min, double max) { return random((double)min, max); }
+double GA::random(double min, int max) { return random(min, (double)max); }
 double GA::random(double min, double max)
 {
 	std::random_device rnd;
@@ -190,32 +162,27 @@ double GA::random(double min, double max)
 	return distribution(engine);
 }
 
-bool GA::displayValues(bool enableOneLine)
+void GA::displayValues(bool enableOneLine)
 {
+	std::vector<Data> data_temp;
 	if (enableOneLine)
 	{
-		for (int j = 0; j < var_num; j++)
-		{
-			printf_s("%10.7lf,", eliteData.x[j]);//デバッグ用
-		}
-		printf_s(" \t f(x,y)=%10.7lf\t Result=%10.7lf\n", eliteData.functionValue, eliteData.result);
-
+		data_temp.push_back(eliteData);
 	}
 	else
 	{
-		std::vector<Data> data_temp = data;
+		data_temp = data;
 		std::sort(data_temp.begin(), data_temp.end(), [](const Data& x, const Data& y) { return x.functionValue > y.functionValue; });
-
-		for (int i = 0; i < max_genom_list; i++)
-		{
-			for (int j = 0; j < var_num; j++)
-			{
-				printf_s("%10.7lf,", data_temp[i].x[j]);//デバッグ用
-			}
-			printf_s(" \t f(x,y)=%10.7lf\t Result=%10.7lf\n", data_temp[i].functionValue, data_temp[i].result);
-		}
 	}
-	return true;
+
+	for (int i = 0; i < data_temp.size(); i++)
+	{
+		for (int j = 0; j < data_temp[i].x.size(); j++)
+		{
+			printf_s("%10.7lf,", data_temp[i].x[j]);//デバッグ用
+		}
+		printf_s(" \t f(x,y)=%10.7lf\t Result=%10.7lf\n", data_temp[i].functionValue, data_temp[i].result);
+	}
 }
 
 GA::Data GA::searchRank(int num)
@@ -224,8 +191,6 @@ GA::Data GA::searchRank(int num)
 	std::sort(data_temp.begin(), data_temp.end(), [](const Data& x, const Data& y) { return x.functionValue < y.functionValue; });
 	return data_temp[num];
 }
-
-
 
 GA::~GA()
 {
--- report2_git_2/report2_git_2/GA.h
+++ report2_git_2/report2_git_2/GA.h
@@ -7,14 +7,13 @@
 #include <algorithm>
 #include <cmath>
 
+#define __ENABLE_SINGLE_POINT_MUTATION__
+
 class GA
 {
 private:
-	int max_genom_list;//個体数
-	int var_num;//品物の個数
 	double individualMutationRate = 0.05;//個体突然変異率
 	double genomMutationRate = 0.1;
-	int minNum = 0, maxNum = 0;
 	double alpha = 0.3;
 	std::vector<double> varMax, varMin;//変数の最小値・最大値
 	bool isChanged = false;
@@ -23,8 +22,6 @@ public:
 
 	class Data//データ格納用クラス
 	{
-	private:
-		int var_num;//変数の数
 	public:
 		std::vector<double> x;//座標
 		double functionValue;//与えられた関数の値
@@ -32,9 +29,7 @@ public:
 
 		Data(int _var_num)//コンストラクタ
 		{
-			var_num = _var_num;
-
-			x.resize(var_num);//isIncludedの配列の長さの設定
+			x.resize(_var_num);//isIncludedの配列の長さの設定
 		}
 	};
 
@@ -42,17 +37,16 @@ public:
 	Data eliteData;
 	GA(int _max_genom_list, int _var_num, std::vector<double> _varMax, std::vector<double> _varMin);	//コンストラクタ
 	bool selection();//選択
-
-	bool blxAlphaCrossover();
-	bool mutation();//突然変異
-	bool calc(bool enableDisplay, bool enableOneLine = false);//評価関数の計算
+	void blxAlphaCrossover();
+	void mutation();//突然変異
+	void calc(bool enableDisplay, bool enableOneLine = false);//評価関数の計算
 private:
-	bool calcResult(bool enableSort = false);
+	void calcResult(bool enableSort = false);
 	int random(int min, int max);
 	double random(int min, double max);
 	double random(double min, int max);
 	double random(double min, double max);
-	bool displayValues(bool enableOneLine);
+	void displayValues(bool enableOneLine);
 	Data searchRank(int num);
 public:
 	~GA();//デコンストラクタ
